a4d024125402d21f24a4a1d87daec4d0fc8b7262
Dmitry Ivanov <di@dostavista.ru>
2020-11-05 16:59:18 +0300

    connectApp hook and AppProvider provider were added.

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,12 +1,13 @@
 import React, { createContext, useState } from "react";
 import { IApp } from "@feature-framework/core";
+import DefaultApplication from '../DefaultApplication';
 
 type AppProviderPropsType = {
   app: IApp;
   children: JSX.Element | JSX.Element[];
 };
 
-export const AppContext = createContext<IApp>();
+export const AppContext = createContext<IApp>(new DefaultApplication({}));
 
 // This context provider is passed to any component requiring the context
 export const AppProvider = ({ app, children }: AppProviderPropsType) => {
