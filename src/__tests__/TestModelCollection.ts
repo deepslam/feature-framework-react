6e9c09e55b59d713a1e58328434363aa789a9440
Dmitry Ivanov <deepslam@gmail.com>
2020-11-16 09:21:31 +0300

    Some useful edits

--- src/__tests__/TestModelCollection.ts
+++ src/__tests__/TestModelCollection.ts
@@ -1,4 +1,4 @@
 import { DataCollection } from "@feature-framework/core";
-import TestModel from "TestModel";
+import TestModel from "./TestModel";
 
 export default class TestModelCollection extends DataCollection<TestModel> {}
